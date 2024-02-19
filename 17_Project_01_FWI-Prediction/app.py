c0d265da0f32d186de34c43ebbabc896d49fecc3
Prateekk02 <prateek.kumar2002@gmail.com>
2024-02-19 20:44:19 +0530

    updated FWI project

--- 17_Project_01_FWI-Prediction/app.py
+++ 17_Project_01_FWI-Prediction/app.py
@@ -13,22 +13,21 @@ standard_scaler = pickle.load(open('models/scaler.pkl','rb'))
 
 # Route for homepage
 
-@app.route('/')
-def index():
-    return render_template('index.html')
-
-@app.route('/predictdata', methods=['GET','POST'])
+@app.route('/', methods=['GET','POST'])
 def predict_datapoint():
     if request.method == 'POST':
-        Temperature = float(request.form.get('Temperature'))
-        RH = float(request.form.get('RH'))
-        Ws = float(request.form.get('Ws'))
-        Rain = float(request.form.get('Rain'))
-        FFMC = float(request.form.get('FFMC'))
-        DMC = float(request.form.get('DMC'))
-        ISI = float(request.form.get('ISI'))
-        Classes = float(request.form.get('Classes'))
-        Region = float(request.form.get('Region'))
+        try:
+            Temperature = float(request.form.get('Temperature'))
+            RH = float(request.form.get('RH'))
+            Ws = float(request.form.get('Ws'))
+            Rain = float(request.form.get('Rain'))
+            FFMC = float(request.form.get('FFMC'))
+            DMC = float(request.form.get('DMC'))
+            ISI = float(request.form.get('ISI'))
+            Classes = float(request.form.get('Classes'))
+            Region = float(request.form.get('Region'))
+        except TypeError:
+            return render_template('/home.html', result="Enter the form again")
         
         # Now we scale our data
         new_scaled_data = standard_scaler.transform([[Temperature,RH,Ws,Rain,FFMC,DMC,ISI,Classes,Region]])
